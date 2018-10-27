944a11f2869b14f6d847328996df1d57faa7b868
MirkoBernardini <mirko@lotrek.it>
2018-10-27 16:38:56 +0200

    implementation cattp

--- cattp/http.py
+++ cattp/http.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
 
 
+CATTP_RESPONSE = """
+<body style="
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: black;">
+    <p style="font-size: 7em;">🐱</p>
+    <img src="https://http.cat/{0}"/>
+    <p style="font-size: 7em;">🐱</p>
+</body>
+"""
+
+
 class HttpCatResponse(HttpResponse):
     
     def __init__(self, status_code=200):
-        return super(HttpResponse, self).__init__(
-            '<img src="https://http.cat/{0}"/>'.format(status_code),
+        super().__init__(
+            content=CATTP_RESPONSE.format(status_code),
             status=status_code
         )
